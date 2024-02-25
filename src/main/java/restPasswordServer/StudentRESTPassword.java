d65d2aa5d3b6e82793fd6097f45b3770084236cf
Michael Bradshaw <michael.bradshaw@centre.edu>
2024-02-25 15:49:14 -0500

    Created expected student test of password server

---
+++ src/main/java/restPasswordServer/StudentRESTPassword.java
@@ -0,0 +1,30 @@
+package restPasswordServer;
+
+import org.springframework.web.client.RestClient;
+
+public class StudentRESTPassword
+{
+
+	public static void main(String[] args)
+	{
+		RestClient client = RestClient.create();
+		
+		String uriBase = "http://localhost:8080/";
+		
+		
+		String pw = client.get()
+		.uri(uriBase+"request/mkb")
+		.retrieve()
+		.body(String.class);
+
+		String response = client.get()
+		.uri(uriBase+"auth/mkb/"+pw)
+		.retrieve()
+		.body(String.class);
+
+		System.out.println(response);
+		
+		
+	}
+
+}
